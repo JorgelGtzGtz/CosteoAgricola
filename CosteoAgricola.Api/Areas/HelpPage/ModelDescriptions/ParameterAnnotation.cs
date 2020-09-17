d36c781fc3d2df85bfe9d62685687ce4d29ddadf
Jorge Gutierrez <jorgelgtzgtz@hotmail.com>
2020-09-16 22:22:26 -0700

    Api Project

---
+++ CosteoAgricola.Api/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CosteoAgricola.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
