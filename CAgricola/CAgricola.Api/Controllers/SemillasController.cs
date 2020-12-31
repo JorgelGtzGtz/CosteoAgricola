f5c077f8718c7f5b3f6197bf2c758d8ce647457b
Hannia <hanniaelin@gmail.com>
2020-12-30 21:38:21 -0700

    Nueva Api

---
+++ CAgricola/CAgricola.Api/Controllers/SemillasController.cs
@@ -0,0 +1,22 @@
+﻿using dbconnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CAgricola.Api.Controllers
+{
+    [Route("api/Semillas")]
+    public class SemillasController : ApiController
+    {
+        dbconnectionDB DB = new dbconnectionDB();
+        [HttpPost]
+        public void Post(int id, string desc,int unidad,int tipo_ins, int upm, float pm,string variedad,int costo,float existencia,bool status,bool inven,string obs)
+        {
+            DB.Query<SEMILLA>("INSERT INTO SEMILLAS(sem_ID,sem_desc,sem_unidad,tipo_insumo,sem_upm,sem_pm,sem_variedad,sem_costoProm,sem_existencia,sem_status,sem_inventariable,sem_obs)" +
+                "VALUES("+id+","+desc+","+unidad+","+tipo_ins+","+upm+","+pm+","+variedad+","+costo+","+existencia+","+status+","+inven+","+obs+")");
+        }
+    }
+}
